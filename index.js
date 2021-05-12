6697c75979afbd4dbe73b12f9db2a977a77cce74
Kape_Roblochon <77213938+KapeRoblochon@users.noreply.github.com>
2021-05-12 22:30:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const client = new Discord.Client();
 
 client.login(process.env.TOKEN)
 
-
 client.on('message', async message => {
-    if(message.content.includes('https://') || message.content.includes('http://') || message.content.includes('www.') || message.content.includes('.fr') || message.content.includes('.net') || message.content.includes('.com') || message.content.includes('.net') || message.content.includes('.gg')){
-        if(message.member.hasPermission('MANAGE_MESSAGES')) return;
-        message.delete()
+    if(message.content.startsWith('https://tenor.com/')) return;
+    else {
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
+        if(message.content.includes('https://') || message.content.includes('http://') || message.content.includes('www.') || message.content.includes('.fr') || message.content.includes('.net') || message.content.includes('.com') || message.content.includes('.gg') || message.content.includes('.com') || message.content.includes('.gouv') || message.content.includes('.org')){
+            message.delete()
     }
+    }
+})
 
 
 })
 
 client.on('ready', () => {
     console.log("bot go")
+    client.user.setActivity('filtrer les liens')
 })
